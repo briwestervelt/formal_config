5223b01f1446ce33e4bdc3a4f4637bc10fa50f87
briwestervelt <briwestervelt@gmail.com>
2016-03-17 22:10:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# formal_config
+
+This is the configuration page for the "Formal" watch face 
---
+++ config/js/main.js
@@ -0,0 +1,76 @@
+(function(){
+  loadOptions();
+  submitHandler();
+})();
+
+function submitHandler(){
+  $("#submitButton").on("click", function(){
+      var return_to = getQueryParam('return_to', 'pebblejs://close#');
+      document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigureData()));
+  });
+}
+
+function loadOptions() {
+  
+  var $backgroundColorPicker = $("#backgroundColorPicker");
+  var $tickColorPicker = $("#tickColorPicker");
+  var $hourColorPicker = $("#hourColorPicker");
+  var $minuteColorPicker = $("#minuteColorPicker");
+  var $dotColorPicker = $("#dotColorPicker");
+  var $dateColorPicker = $("#dateColorPicker");
+  var $bluetoothVibesCheckbox = $("#bluetoothVibesCheckbox");
+
+  if (localStorage.backgroundColor) {
+    $backgroundColorPicker[0].value = localStorage.backgroundColor;
+    $tickColorPicker[0].value = localStorage.foregroundColor;
+    $hourColorPicker[0].value = localStorage.foregroundColor;
+    $minuteColorPicker[0].value = localStorage.foregroundColor;
+    $dotColorPicker[0].value = localStorage.foregroundColor;
+    $dateColorPicker[0].value = localStorage.foregroundColor;
+    $bluetoothVibesCheckbox[0].checked = localStorage.bluetoothVibes === "true";
+  }
+}
+
+function getAndStoreConfigureData(){
+
+  var $backgroundColorPicker = $("#backgroundColorPicker");
+  var $tickColorPicker = $("#tickColorPicker");
+  var $hourColorPicker = $("#hourColorPicker");
+  var $minuteColorPicker = $("#minuteColorPicker");
+  var $dotColorPicker = $("#dotColorPicker");
+  var $dateColorPicker = $("#dateColorPicker");
+  var $bluetoothVibesCheckbox = $("#bluetoothVibesCheckbox");
+
+  var options = {
+    backgroundColor: $backgroundColorPicker.val(),
+    tickColor: $tickColorPicker.val(),
+    hourColor: $hourColorPicker.val(),
+    minuteColor: $minuteColorPicker.val(),
+    dotColor: $dotColorPicker.val(),
+    dateColor: $dateColorPicker.val(),
+    bluetoothVibes: $bluetoothVibesCheckbox[0].checked,
+  }
+
+  localStorage.backgroundColor = options.backgroundColor;
+  localStorage.tickColor = options.tickColor;
+  localStorage.hourColor = options.hourColor;
+  localStorage.minuteColor = options.minuteColor;
+  localStorage.dotColor = options.dotColor;
+  localStorage.dateColor = options.dateColor;
+  localStorage.bluetoothVibes = options.bluetoothVibes;
+
+  console.log("Got options " + JSON.stringify(options));
+  return options;
+}
+
+function getQueryParam(variable, defaultValue) {
+  var query = location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    if (pair[0] === variable) {
+      return decodeURIComponent(pair[1]);
+    }
+  }
+  return defaultValue || false;
+}
